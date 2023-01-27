086cb1c83a70b1c077aec675603124cb29b230c3
TanoRG <tano.tropea.rg@outlook.com>
2023-01-26 23:46:11 -0300

    Se cambiaron los textos de los botones ocultar/mostrar, por iconos de fontawesome

--- app.js
+++ app.js
@@ -1,51 +1,24 @@
 var botonExpOjs = document.getElementById("botonExpO");
-var botonExpMjs = document.getElementById("botonExpM");
 var ocultar_exp = document.getElementById("ExperienciaLaboralJS")
 var botonEstOjs = document.getElementById("botonEstO");
-var botonEstMjs = document.getElementById("botonEstM");
 var ocultar_est = document.getElementById("EstudiosJS")
 
-
-/* Botones Ocultar y Mostrar ExpLab, Originales. *
 botonExpOjs.addEventListener("click", function () {
-    console.log("Ocultamos la sección Experiencia Laboral");
-    ocultar_exp.style.display = 'none';
-});
-
-botonExpMjs.addEventListener("click", function () {
-    console.log("Volvemos a mostrar la sección Esperiencia Laboral");
-    ocultar_exp.style.display = '';
-});
-*/
-
-botonExpOjs.addEventListener("click", function () { 
     if(ocultar_exp.style.display === 'none') {
-        ocultar_exp.style.display = '';
-        botonExpOjs.innerHTML = "Ocultar";
+    ocultar_exp.style.display = '';
+    botonExpOjs.innerHTML = "<i class='fa-regular fa-circle-xmark'></i>";
     } else {
-        ocultar_exp.style.display = 'none';
-        botonExpOjs.innerHTML = "Mostrar";
+    ocultar_exp.style.display = 'none';
+    botonExpOjs.innerHTML = "<i class='fa-regular fa-circle-down'></i>";
     }
-});
-
-/* Botones Ocultar y Mostrar Estudios, Originales. *
-botonEstOjs.addEventListener("click", function () {
-    console.log("Ocultamos la sección Estudios");
-    ocultar_est.style.display = 'none';
-});
-
-botonEstMjs.addEventListener("click", function () {
-    console.log("Volvemos a mostrar la sección Estudios");
-    ocultar_est.style.display = '';
-});
-*/
+    });
 
 botonEstOjs.addEventListener("click", function () { 
     if(ocultar_est.style.display === 'none') {
         ocultar_est.style.display = '';
-        botonEstOjs.innerHTML = "Ocultar";
+        botonEstOjs.innerHTML = "<i class='fa-regular fa-circle-xmark'></i>";
     } else {
         ocultar_est.style.display = 'none';
-        botonEstOjs.innerHTML = "Mostrar";
+        botonEstOjs.innerHTML = "<i class='fa-regular fa-circle-down'></i>";
     }
 });
